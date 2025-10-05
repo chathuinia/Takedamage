347e90c91f9c647ccadd65f6c92b07eca1100b63
chathuinia <datlovsemen53@gmail.com>
2025-10-05 11:41:44 +0300

    Platform.cs

--- Platform.cs
+++ Platform.cs
@@ -1,7 +1,25 @@
-public class Platform :
-MonoBehaviour
+public class Platform : MonoBehaviour
 {
     public float speed;
     public Vector3 direction;
     public bool isActive;
+
+    void Update()
+    {
+        if (isActive)
+        {
+            transform.position += direction * speed * Time.deltaTime;
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "PlatformStop")
+        {
+            direction *= -1;
+        }
+        if(other.tag == "Player")
+        {
+            isActive = true;
+        }
+    }
 }
